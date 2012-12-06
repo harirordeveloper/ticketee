1efd99e567e14d0dca38441d33118efd6949882b
Hari Krishna <harirordeveloper@gmail.com>
2012-12-06 11:12:43 +0530

    INITIAL COMMIT

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+gem 'sqlite3'
+
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
+
+
+group :test do
+  gem 'cucumber-rails'
+  gem 'capybara','0.4.1.2'
+  gem 'factory_girl'
+  gem 'email_spec'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketee::Application.config.secret_token = '64ef11dfc0a73e915b5117aae0f0c11029fbe7e0240d38e38da66b03e8d8b7aadfe0796ecfd490d8e0ef4804dda252258dc1cf59c63714292bbcd062a70b83bf'
